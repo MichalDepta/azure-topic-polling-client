74fd45d6dd2721c329578c89f72869237ab3d162
Michał Depta <mdepta@future-processing.com>
2016-03-08 21:27:21 +0100

    Created an Azure ServiceBus Topic producer and a polling consumer

--- ServiceBusTopicConsumer/Program.cs
+++ ServiceBusTopicConsumer/Program.cs
@@ -1,15 +1,91 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
 
 namespace ServiceBusTopicConsumer
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const string TopicName = "TestTopic";
+        private const string SubscriptionName = "TestSubscription";
+
+        public static void Main(string[] args)
         {
+            var client = CreateSubscriptionClient();
+            Console.WriteLine("Enter messages to process:");
+
+            client.OnMessage(message =>
+            {
+                var content = message.Properties["content"];
+                Console.WriteLine($"=> {content}");
+            }, new OnMessageOptions {AutoComplete = false});
+
+            try
+            {
+                while (true)
+                {
+                    var line = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+
+                    if (ShouldQuit(line))
+                    {
+                        break;
+                    }
+
+                    MatchWithTopicItem(line, client);
+                }
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+
+        private static bool ShouldQuit(string input) => "quit".Equals(input, StringComparison.OrdinalIgnoreCase);
+
+        private static void MatchWithTopicItem(string input, SubscriptionClient client)
+        {
+            for (var i = 0;; i++)
+            {
+                var message = client.Peek(i);
+
+                if (message == null)
+                {
+                    break;
+                }
+
+                var content = message.Properties["content"];
+
+                if (content.Equals(input))
+                {
+                    // The following raises an InvalidOperationException:
+                    message.Complete(); 
+                    break;
+                }
+            }
+        }
+
+        private static SubscriptionClient CreateSubscriptionClient()
+        {
+            var connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            if (!namespaceManager.TopicExists(TopicName))
+            {
+                namespaceManager.CreateTopic(TopicName);
+            }
+
+            if (!namespaceManager.SubscriptionExists(TopicName, SubscriptionName))
+            {
+                namespaceManager.CreateSubscription(TopicName, SubscriptionName);
+            }
+
+            return SubscriptionClient.CreateFromConnectionString(connectionString, TopicName, SubscriptionName, ReceiveMode.PeekLock);
         }
     }
 }
--- ServiceBusTopicProducer/Program.cs
+++ ServiceBusTopicProducer/Program.cs
@@ -1,15 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
 
 namespace ServiceBusTopicProducer
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const string TopicName = "TestTopic";
+        private const string SubscriptionName = "TestSubscription";
+
+        public static void Main(string[] args)
         {
+            var client = CreateTopicClient();
+
+            Console.WriteLine("Enter messages to send:");
+
+            try
+            {
+                while (true)
+                {
+                    var line = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+
+                    if (ShouldQuit(line))
+                    {
+                        break;
+                    }
+
+                    var message = CreateMessage(line);
+                    client.Send(message);
+                }
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+
+        private static bool ShouldQuit(string input) => "quit".Equals(input, StringComparison.OrdinalIgnoreCase);
+
+        private static BrokeredMessage CreateMessage(string content)
+        {
+            var message = new BrokeredMessage();
+            message.Properties["content"] = content;
+            return message;
+        }
+
+        private static TopicClient CreateTopicClient()
+        {
+            var connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            if (!namespaceManager.TopicExists(TopicName))
+            {
+                namespaceManager.CreateTopic(TopicName);
+            }
+
+            return TopicClient.CreateFromConnectionString(connectionString, TopicName);
         }
     }
 }
